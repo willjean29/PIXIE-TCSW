da92cac162741f12587e4942be4ac36f3d6b093b
Jean Osco <willjean29@gmail.com>
2020-08-17 23:38:33 -0500

    Controlador Cliente

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,37 +1,37 @@
-// const Logger = require('../config/loggerService');
-// const logger = new Logger('app');
-// const microprofiler = require('microprofiler');
-// const Business = require('../models/Business');
-// const Client = require('../models/Client');
-// const mostrarClientesTotales = async(req,res) => {
-//   let start = microprofiler.start();
-//   let clientes = await clientesTotales(req.user.id);
-//   // console.log(clientes)
-//   res.render('admin/listar-clientes-totales',{
-//     title: "Clientes",
-//     clientes
-//   })
-//   let elapsedUs = microprofiler.measureFrom(start,'code');
-//   let stats = microprofiler.getStats('code');
-//   logger.debug('Procesar request mostrarClientesTotales',stats)
-// }
+const Logger = require('../config/loggerService');
+const logger = new Logger('app');
+const microprofiler = require('microprofiler');
+const Business = require('../models/Business');
+const Client = require('../models/Client');
+const mostrarClientesTotales = async(req,res) => {
+  let start = microprofiler.start();
+  let clientes = await clientesTotales(req.user.id);
+  // console.log(clientes)
+  res.render('admin/listar-clientes-totales',{
+    title: "Clientes",
+    clientes
+  })
+  let elapsedUs = microprofiler.measureFrom(start,'code');
+  let stats = microprofiler.getStats('code');
+  logger.debug('Procesar request mostrarClientesTotales',stats)
+}
 
-// const clientesTotales = async(id) => {
-//   let clientesActuales = [];
-//   const business = await Business.findOne({administrador: id});
-//   const {clientes} = business;
-//   for (let cliente of clientes) {
-//     let clienteInfo = await Client.findById(cliente.idCliente).lean();
-//     for (let puntuacion of clienteInfo.puntuacion) {
-//       if(JSON.stringify(puntuacion.idBusiness) === JSON.stringify(business._id)){
-//         clienteInfo.puntuacion = puntuacion.puntos;
-//       }
-//     }
-//     clientesActuales.push(clienteInfo);
-//   }
-//   return clientesActuales;
-// }
+const clientesTotales = async(id) => {
+  let clientesActuales = [];
+  const business = await Business.findOne({administrador: id});
+  const {clientes} = business;
+  for (let cliente of clientes) {
+    let clienteInfo = await Client.findById(cliente.idCliente).lean();
+    for (let puntuacion of clienteInfo.puntuacion) {
+      if(JSON.stringify(puntuacion.idBusiness) === JSON.stringify(business._id)){
+        clienteInfo.puntuacion = puntuacion.puntos;
+      }
+    }
+    clientesActuales.push(clienteInfo);
+  }
+  return clientesActuales;
+}
 
-// module.exports = {
-//   mostrarClientesTotales
-// }
+module.exports = {
+  mostrarClientesTotales
+}
