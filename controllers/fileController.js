3859de7ec15a07ecc3e129543e78b34493b455a6
Melinna <melin1504@gmail.com>
2020-08-19 23:56:43 -0500

    Modicando FileController eliminar Archivo

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,6 +1,7 @@
 const File = require('../models/File');
 const Business = require('../models/Business');
 const {leerCSV} = require('../utils/leerCSV');
+
 const registrarArchivo = async(req,res) => {
   let file;
   const id = req.user._id;
@@ -58,7 +59,42 @@ const obtenerDatosArchivo = async(req,res) => {
   })
 
 }
+
+const eliminarArchivo = async(req,res) => {
+  let file;
+  const id = req.user._id;
+  const business = await Business.findOne({administrador: id}).catch((err) => {
+    return res.status(400).json({
+      ok: false,
+      err
+    })
+  });
+
+  if(!business) return res.status(400).json({
+    ok: false,
+    err: {
+      msg: "La empresa no se encuentra registrada"
+    }
+  })
+
+  if(req.file){
+    file = new File({
+      name: req.file.filename,
+      type: req.file.mimetype,
+      business: business._id
+    })
+
+    await file.save();
+  }
+
+  res.json({
+    ok: true,
+    file
+  })
+
+}
 module.exports = {
   registrarArchivo,
-  obtenerDatosArchivo
+  obtenerDatosArchivo,
+  eliminarArchivo
 }
