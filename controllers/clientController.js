5dd9498820d961f3c9e1130f6b6c7aa8a8d6364d
Melinna <melin1504@gmail.com>
2020-08-31 20:05:00 -0500

    Modifying Controller

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,5 +1,7 @@
 const Category = require('../models/Category');
 //Registrar Categoria
+
+//Realizando función eliminar
 const registrarCategoria = async(req,res) => {
   const data = req.body;
   console.log("hola categoey")
@@ -17,4 +19,23 @@ const registrarCategoria = async(req,res) => {
     category
   });
 }
+
+const eliminarCategoria = async(req,res) => {
+  const data = req.body;
+  console.log("hola categoey")
+  console.log(data)
+  const category = new Category(data);
+  await category.save().catch((err) => {
+    return res.status(400).json({
+      ok: false,
+      err
+    })
+  })
+
+  res.json({
+    ok: true,
+    category
+  });
+}
+
 
